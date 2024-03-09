484b40043a25763d2cfb096b779d2a501d7b764f
moljac <mcvjetko@holisticware.net>
2024-03-09 20:53:07 +0100

    renamed file

--- source/HolisticWare.Tools.Aspire.Hosting.Clients.Python/IDistributedApplicationBuilderExtensions.cs
+++ source/HolisticWare.Tools.Aspire.Hosting.Clients.Python/IDistributedApplicationBuilderExtensions.cs
@@ -52,4 +52,28 @@ public static partial class
 
         return resource_builder;
     }
+    
+    public static
+        IResourceBuilder<ExecutableResource>
+                                        AddScriptPythonFlask
+                                        (
+                                            this IDistributedApplicationBuilder? builder,
+                                            string name,
+                                            string path_python,
+                                            string working_directory,
+                                            string[] python_script_and_args,
+                                            string aspire_settings = "aspire_settings.py"
+                                        )
+    {
+        IResourceBuilder<ExecutableResource>? resource_builder = AddScriptPython
+                                                                        (
+                                                                            builder,
+                                                                            name,
+                                                                            path_python,
+                                                                            working_directory,
+                                                                            python_script_and_args
+                                                                        );
+
+        return resource_builder;
+    }
 }
